288552444cde460a83d17e2af6a910e4a0a84c9b
Guo Li <leeguoo@hotmail.com>
2017-07-18 14:55:56 -0700

    test

---
+++ src/1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('../local/train_1.csv',index_col='Page',nrows=1000)
+
+df = df.stack().reset_index()
+
+df["date"] = pd.to_datetime(df.level_1)
+df["year"] = df.date.dt.year
+df["month"] = df.date.dt.month
+df["day"] = df.date.dt.day
+df["weekday"] = df.date.dt.dayofweek
+print(df)
