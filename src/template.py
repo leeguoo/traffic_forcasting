e40416715ae32d42695ce81cb857039a7dc82791
Guo Li <leeguoo@hotmail.com>
2017-07-18 15:17:17 -0700

    parse daytime,language, and content

--- src/template.py
+++ src/template.py
@@ -9,4 +9,14 @@ df["year"] = df.date.dt.year
 df["month"] = df.date.dt.month
 df["day"] = df.date.dt.day
 df["weekday"] = df.date.dt.dayofweek
+
+df["lang"] = df.Page.map(lambda x: x.split(".wikipedia.org_")[0].split("_")[-1])
+df["title"] = df.Page.map(lambda x: x.split(".wikipedia.org_")[0].split("_")[0])
+df["access"] = df.Page.map(lambda x: x.split(".wikipedia.org_")[1].split("_")[0])
+df["agent"] = df.Page.map(lambda x: x.split(".wikipedia.org_")[1].split("_")[1])
+
+df["Page"] = df.Page+df.level_1
+df.drop(["level_1"],axis=1,inplace=True)
+df.rename(columns:{0:"traffic"})
+
 print(df)
