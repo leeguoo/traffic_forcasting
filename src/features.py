f008f0fb96e002c9a108fe49c852723794728602
Guo Li <leeguoo@hotmail.com>
2017-08-08 21:14:51 -0700

    feature generation script

--- src/features.py
+++ src/features.py
@@ -4,11 +4,25 @@ import numpy as np
 
 class WebTraffic(object):
     def __init__(self,trainpath):
-        self.raw = pd.read_csv(trainpath,index_col='Page',nrows=5000)
-#        self.raw = self.raw.fillna(0)
+        for df in pd.read_csv(trainpath,chunksize=200,nrows=1000):
 
-        self.df = pd.DataFrame()
-        self.df["traffic"] = self.raw.stack().map(np.log1p)
+            #self.raw = pd.read_csv(trainpath,index_col='Page',nrows=1)
+            start = self.raw.columns.max()
+            end = '2017-03-01'
+    
+            self.raw = pd.concat([self.raw,
+                                  pd.DataFrame(columns=pd.date_range(start,end)[1:]
+                                                         .map(lambda t: t.strftime('%Y-%m-%d')))])
+            self.raw = self.raw.T.T
+    
+            self.df = pd.DataFrame()
+            self.df["traffic"] = self.raw.stack(dropna=False).map(np.log1p)
+    
+            self.RunAll()
+            
+            f = open('../../data/data.csv','a')
+            self.df.to_csv(f,index=False,header=False)
+            f.close()
 
     def RunAll(self):
         self.WKMediaLag()
@@ -18,7 +32,8 @@ class WebTraffic(object):
         self.Access()
         self.Lang()
         self.df = self.df[self.df.index.get_level_values(1)>='2016-01-01']
-#        self.df = self.df.dropna(how='any')
+        self.df = self.df.reset_index()
+        self.df = self.df.rename(columns={'level_0':'Page','level_1':'Date'})
 
     def WKMediaLag(self, lags=[61,91,121,151,181]):
         weekmedian = self.raw.rolling(7,axis=1).median()
@@ -26,18 +41,18 @@ class WebTraffic(object):
         monthaverage = weekmedian.rolling(28,axis=1).mean()
         for num in lags:
             tag = "lag_{0}".format(num)
-            self.df[tag] = self.raw.shift(num,axis=1).stack()
-            self.df["WKM_"+tag] = weekmedian.shift(num,axis=1).stack()
-            self.df["BWKA_"+tag] = biweekaverage.shift(num,axis=1).stack()
-            self.df["MA_"+tag] = monthaverage.shift(num,axis=1).stack()
+            self.df[tag] = self.raw.shift(num,axis=1).stack(dropna=False)
+            self.df["WKM_"+tag] = weekmedian.shift(num,axis=1).stack(dropna=False)
+            self.df["BWKA_"+tag] = biweekaverage.shift(num,axis=1).stack(dropna=False)
+            self.df["MA_"+tag] = monthaverage.shift(num,axis=1).stack(dropna=False)
 
     def LagFea(self, nums=range(7,63,7),lags=[61,91,121]):
         for num in nums:
             for lag in lags:
                 meantag = "mean_{0}_lag_{1}".format(num,lag)
-                self.df[meantag] = self.raw.rolling(num,axis=1).mean().shift(lag,axis=1).stack()
+                self.df[meantag] = self.raw.rolling(num,axis=1).mean().shift(lag,axis=1).stack(dropna=False)
                 mediantag = "median_{0}_lag_{1}".format(num,lag)
-                self.df[mediantag] = self.raw.rolling(num,axis=1).median().shift(lag,axis=1).stack()
+                self.df[mediantag] = self.raw.rolling(num,axis=1).median().shift(lag,axis=1).stack(dropna=False)
 
     def SeasonLag(self,lags=[61,91]):
         tmp = self.raw.T
@@ -57,8 +72,8 @@ class WebTraffic(object):
                  tmpMean = gp.rolling(num).mean().drop(tag,axis=1).reset_index().sort_values("level_1").set_index("level_1")[cols].T
                  tmpMedian = gp.rolling(num).median().drop(tag,axis=1).reset_index().sort_values("level_1").set_index("level_1")[cols].T
                  for lag in lags:
-                     self.df["{0}_mean_{1}_{2}".format(tag,num,lag)] = tmpMean.shift(lag,axis=1).stack()
-                     self.df["{0}_median_{1}_{2}".format(tag,num,lag)] = tmpMedian.shift(lag,axis=1).stack()
+                     self.df["{0}_mean_{1}_{2}".format(tag,num,lag)] = tmpMean.shift(lag,axis=1).stack(dropna=False)
+                     self.df["{0}_median_{1}_{2}".format(tag,num,lag)] = tmpMedian.shift(lag,axis=1).stack(dropna=False)
 
     def Agent(self):
         tmp = pd.get_dummies(self.df.index.get_level_values(0).map(lambda x: x.lower().split("_")[-1]),
@@ -87,43 +102,3 @@ class WebTraffic(object):
 
 trainpath = '../../input/train_1.csv'
 WT = WebTraffic(trainpath)
-WT.RunAll()
-df = WT.df
-
-#find feature and target names
-features = list(df.columns.values)
-target = 'traffic'
-features.remove(target)
-
-#
-#split train and test
-print("split")
-train = df[df.index.get_level_values(1)<='2016-10-23']
-test = df[df.index.get_level_values(1)>='2016-11-01']
-
-train_X, train_y = train[features], train[target]
-test_X, test_y = test[features], test[target]
-
-print("model")
-
-from xgboost import XGBRegressor
-from smape import XGBsmape, smape
-
-xgbr = XGBRegressor(max_depth=9, 
-                    learning_rate=0.05, 
-                    n_estimators=1000,
-                    silent=True, 
-                    objective='reg:linear', 
-                    nthread=-1, 
-                    subsample=0.8, 
-                    colsample_bytree=0.8)
-
-xgbr.fit(train_X,train_y,
-         eval_set=[(train_X,train_y),(test_X,test_y)],
-         eval_metric=XGBsmape,
-         early_stopping_rounds=10,
-         verbose=True)
-
-print(smape(np.expm1(test_y),np.expm1(xgbr.predict(test_X))))
-
-print sum(np.expm1(xgbr.predict(test_X))<0)
